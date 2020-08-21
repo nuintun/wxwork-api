45c0a03b97264fe29c8d2f2c4d6c65491a50f466
nuintun <nuintun@qq.com>
2020-08-21 09:59:07 +0800

    :arrow_up: Bump deps

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+/**
+ * @module rollup
+ * @license MIT
+ * @version 2017/10/24
+ */
+
+'use strict';
+
+import pkg from './package.json';
+
+const banner = `/**
+ * @module ${pkg.name}
+ * @author ${pkg.author.name}
+ * @license ${pkg.license}
+ * @version ${pkg.version}
+ * @description ${pkg.description}
+ * @see ${pkg.homepage}
+ */
+`;
+
+export default {
+  input: 'src/index.js',
+  output: {
+    banner,
+    strict: true,
+    indent: true,
+    format: 'cjs',
+    interop: false,
+    exports: 'auto',
+    esModule: false,
+    file: 'index.js',
+    preferConst: true
+  },
+  external: ['url', 'axios']
+};
